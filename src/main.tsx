0f2305fa00d314418aef087da97f0f3dfe2ca058
yang vieira <yangflix3@gmail.com>
2022-10-21 00:25:35 -0400

    setup inicial

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DashBoard from './pages/Dashboard'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <DashBoard />
+  </React.StrictMode>
+)
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DashBoard: React.FC = () => {
+    return(
+        <h1>DashBoard</h1>
+    )
+}
+
+export default DashBoard;
