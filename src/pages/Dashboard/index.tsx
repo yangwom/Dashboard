df0fa16f2608306137f2ac8b2477444afa763f1a
yang vieira <yangflix3@gmail.com>
2022-10-21 15:23:26 -0400

    pages dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const DashBoard: React.FC = () => {
-    return(
-        <h1>DashBoard</h1>
-    )
-}
+  return <h1>DashBoard</h1>;
+};
 
 export default DashBoard;
