7224c8c65b79425b456f50343d0ab77e635ab699
yang vieira <yangflix3@gmail.com>
2022-10-22 15:34:13 -0400

    content

---
+++ src/Components/Content/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './style';
+
+const Content: React.FC = () => {
+  return (
+    <Container>
+      <h1>Contentx</h1>
+    </Container>
+  );
+};
+
+export default Content;
