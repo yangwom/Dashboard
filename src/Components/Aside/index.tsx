94147929db453c5c12cbf282e3fea162b8e67490
yang vieira <yangflix3@gmail.com>
2022-10-24 08:32:41 -0400

    Aside

--- src/Components/Aside/index.tsx
+++ src/Components/Aside/index.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import {
+  MdArrowBack,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdDashboard,
+  MdExitToApp,
+  } from 'react-icons/md'
 
-import { Container } from './style';
+import {
+  Header,
+  LogImg,
+  Container,
+  MenuConatainer,
+  MenuItemLink,
+  Title,
+} from './style';
+
+import logoImg from '../../assets/dollar.png';
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogImg src={logoImg} alt='Logo minha Carteira'></LogImg>
+        <Title>Minha Carteira</Title>
+      </Header>
+
+      <MenuConatainer>
+        <MenuItemLink href='#'>
+          <MdDashboard />
+          DashBoard
+        
+        </MenuItemLink>
+       
+        <MenuItemLink href='#'>
+          <MdArrowUpward/>
+          Entradas
+          </MenuItemLink>
+        <MenuItemLink href='#'>
+           <MdArrowDownward />
+          Saidas</MenuItemLink>
+        <MenuItemLink href='#'>
+        <MdExitToApp/>
+          Sair</MenuItemLink>
+      </MenuConatainer>
     </Container>
   );
 };
--- src/Components/Aside/style.ts
+++ src/Components/Aside/style.ts
@@ -1,7 +1,46 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-export const Container = styled.div `
-grid-area: AS;
-background-color: #252A48;
 
-`
+export const Container = styled.div`
+  grid-area: AS;
+  background-color: ${(props) => props.theme.colors.secondary};
+  padding-left: 20px;
+  border-right: 1px solid ${props => props.theme.colors.gray};
+`;
+
+export const Header = styled.header`
+height: 70px;
+display: flex;
+align-items: center;
+`;
+export const LogImg = styled.img`
+width: 40px;
+height: 40px;
+`;
+export const Title = styled.h3`
+color: ${ props => props.theme.colors.white};
+margin-left: 10px;
+
+`;
+export const MenuConatainer = styled.nav`
+margin-top: 50px;
+display: flex;
+flex-direction: column;
+`;
+export const MenuItemLink = styled.a`
+color: ${props => props.theme.colors.info};
+display: flex;
+align-items: center;
+text-decoration: none;
+margin: 7px 0;
+
+transition: opacity .3s;
+&:hover {
+    opacity: .7;
+}
+
+> svg {
+    font-size: 18px;
+    margin-right: 5px;
+}
+`;
