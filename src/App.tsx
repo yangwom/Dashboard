c6874e16d658dd6be0cd8bdea4427a1ab2c56926
yang vieira <yangflix3@gmail.com>
2022-10-24 08:30:33 -0400

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import GlobalStyles from './Styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
 import Layout from './Components/Layout';
+import dark from './Styles/theme/dark';
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 };
 
-export default App
+export default App;
